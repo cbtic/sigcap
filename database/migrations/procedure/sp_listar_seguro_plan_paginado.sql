41e3a4bbd5a10fb64b993e9f62fb2bcd0e965f0d
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 <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>
2023-10-21 22:41:19 -0500

    Actualizacion procedures

---
+++ database/migrations/procedure/sp_listar_seguro_plan_paginado.sql
@@ -0,0 +1,57 @@
+CREATE OR REPLACE FUNCTION public.sp_listar_seguro_plan_paginado(p_seguro character varying, p_estado character varying, p_pagina character varying, p_limit character varying, p_ref refcursor)
+ RETURNS refcursor
+ LANGUAGE plpgsql
+AS $function$
+
+Declare
+--v_id numeric;
+--v_numinf character varying;
+v_scad varchar;
+v_campos varchar;
+v_tabla varchar;
+v_where varchar;
+v_count varchar;
+v_col_count varchar;
+--v_perfil varchar;
+
+begin
+	 
+	p_pagina=(p_pagina::Integer-1)*p_limit::Integer;
+	
+	v_campos=' id, nombre,descripcion,estado   ';
+
+	v_tabla=' from seguros_planes ';
+	
+	
+	v_where = ' Where 1=1  ';
+	/*
+	If p_ruc<>'' Then
+	 v_where:=v_where||'And t1.ruc ilike ''%'||p_ruc||'%'' ';
+	End If;
+	
+	
+	*/
+	If p_seguro<>'' Then
+	 v_where:=v_where||'And seguros_planes = '''||p_seguro||''' ';
+	End If;
+
+	If p_estado<>'' Then
+	 v_where:=v_where||'And estado = '''||p_estado||''' ';
+	End If;
+
+	EXECUTE ('SELECT count(1) '||v_tabla||v_where) INTO v_count;
+	v_col_count:=' ,'||v_count||' as TotalRows ';
+
+	If v_count::Integer > p_limit::Integer then
+		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By nombre  LIMIT '||p_limit||' OFFSET '||p_pagina||';'; 
+	else
+		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By nombre ;'; 
+	End If;
+	
+	--Raise Notice '%',v_scad;
+	Open p_ref For Execute(v_scad);
+	Return p_ref;
+End
+
+$function$
+;
