452a6691492b07631e062b09dd5d2b5ef2a78981
Julio Yamunaqué Cruz <julioyamunaque04@gmail.com>
2023-11-14 23:24:22 -0500

    movilidad

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -26,6 +26,8 @@ use App\Models\Certificado;
 use App\Http\Controllers\Frontend\ComisionController;
 
 use App\Http\Controllers\Frontend\PeriodoComisionController;
+use App\Http\Controllers\Frontend\MovilidadController;
+
 
 /*
  * Frontend Controllers
@@ -284,3 +286,9 @@ Route::post('certificado/send_certificado', [CertificadoController::class, 'send
 Route::get('certificado/eliminar_certificado/{id}/{estado}', [CertificadoController::class, 'eliminar_certificado'])->name('certificado.eliminar_certificado');
 Route::get('certificado/certificado_vista/{id}', [CertificadoController::class, 'certificado_vista'])->name('certificado.certificado_vista');
 Route::get('certificado/certificado_pdf/{id}', [CertificadoController::class, 'certificado_pdf'])->name('certificado.certificado_pdf');
+
+Route::get('movilidad/consulta_movilidad', [MovilidadController::class, 'consulta_movilidad'])->name('movilidad.consulta_movilidad');
+Route::post('movilidad/listar_movilidad_ajax', [MovilidadController::class, 'listar_movilidad_ajax'])->name('movilidad.listar_movilidad_ajax');
+Route::get('movilidad/editar_movilidad/{id}', [MovilidadController::class, 'editar_movilidad'])->name('movilidad.editar_movilidad');
+Route::get('movilidad/modal_movilidad_nuevoMovilidad/{id}', [MovilidadController::class, 'modal_movilidad_nuevoMovilidad'])->name('movilidad.modal_movilidad_nuevoMovilidad');
+Route::post('movilidad/send_movilidad_nuevoMovilidad', [MovilidadController::class, 'send_movilidad_nuevoMovilidad'])->name('movilidad.send_movilidad_nuevoMovilidad');
