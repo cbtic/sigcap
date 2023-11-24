83e0f4f1185c9e2dc9aedef4002d3b7aac9e7276
unknown <wyamunaque.expertta@gmail.com>
2023-11-24 00:19:27 -0500

    sesion nueva..

--- database/migrations/procedure/sp_listar_programacion_sesion_paginado.sql
+++ database/migrations/procedure/sp_listar_programacion_sesion_paginado.sql
@@ -1,11 +1,4 @@
-CREATE OR REPLACE FUNCTION public.sp_listar_programacion_sesion_paginado(
-p_id_periodo_comisiones character varying, 
-p_id_tipo_agrupacion character varying, 
-p_comision character varying, 
-p_estado character varying, 
-p_pagina character varying, 
-p_limit character varying, 
-p_ref refcursor)
+CREATE OR REPLACE FUNCTION public.sp_listar_programacion_sesion_paginado(p_id_periodo_comisiones character varying, p_id_tipo_agrupacion character varying, p_comision character varying, p_estado character varying, p_pagina character varying, p_limit character varying, p_ref refcursor)
  RETURNS refcursor
  LANGUAGE plpgsql
 AS $function$
@@ -25,13 +18,12 @@ begin
 	
 	p_pagina=(p_pagina::Integer-1)*p_limit::Integer;
 	
-	v_campos=' c.id,c.denominacion,c.comision,tm.denominacion tipo_agrupacion, cm.monto, '''' estado ';
+	v_campos=' to_char(t1.fecha_programado,''dd-mm-yyyy'')fecha_programado,to_char(t1.fecha_ejecucion,''dd-mm-yyyy'')fecha_ejecucion,
+t1.hora_inicio,t1.hora_fin,t2.denominacion tipo_sesion,t3.denominacion estado_sesion ';
 
-	v_tabla=' from comisiones c
-inner join municipalidad_integradas mi on c.id_municipalidad_integrada = mi.id
-inner join tabla_maestras tm on mi.id_tipo_agrupacion ::int =tm.codigo::int and tm.tipo=''99''
-left join comision_movilidades cm on cm.id_municipalidad_integrada =mi.id ';
-	
+	v_tabla=' from comision_sesiones t1 
+inner join tabla_maestras t2 on t1.id_tipo_sesion::int = t2.codigo::int And t2.tipo =''71''
+inner join tabla_maestras t3 on t1.id_estado_sesion::int = t3.codigo::int And t3.tipo =''56'' ';
 	
 	v_where = ' Where 1=1  ';
 	
@@ -49,9 +41,9 @@ left join comision_movilidades cm on cm.id_municipalidad_integrada =mi.id ';
 	v_col_count:=' ,'||v_count||' as TotalRows ';
 
 	If v_count::Integer > p_limit::Integer then
-		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By c.id desc LIMIT '||p_limit||' OFFSET '||p_pagina||';'; 
+		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By t1.id desc LIMIT '||p_limit||' OFFSET '||p_pagina||';'; 
 	else
-		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By c.id desc ;'; 
+		v_scad:='SELECT '||v_campos||v_col_count||v_tabla||v_where||' Order By t1.id desc ;'; 
 	End If;
 	
 	--Raise Notice '%',v_scad;
@@ -61,4 +53,3 @@ End
 
 $function$
 ;
-
