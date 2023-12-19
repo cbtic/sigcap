a10de9a1b7073b6bcb222ad4c52dbf72d3fc3ffa
Freddy <frimacc@gmail.com>
2023-12-18 21:10:11 -0500

    Create prestamos y detalle prestamo

---
+++ database/migrations/2023_12_19_015813_create_prestamos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrestamosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_agremiado')->nullable();
+            $table->bigInteger('id_periodo_delegado')->nullable();
+            $table->bigInteger('id_tipo_prestamo')->nullable();
+            $table->date('fecha')->nullable();
+            $table->Integer('nro_total_cuotas',10)->nullable();
+            $table->double('total_prestamo',15,8)->nullable();
+            $table->string('estado',1)->nullable()->default('1');
+ 
+            $table->bigInteger('id_usuario_inserta')->unsigned()->index();
+			$table->bigInteger('id_usuario_actualiza')->nullable()->unsigned()->index(); 
+            $table->timestamps();
+
+            $table->foreign('id_agremiado')->references('id')->on('agremiados');
+            $table->foreign('id_periodo_delegado')->references('id')->on('periodo_delegados');
+          //  $table->foreign('id_tipo_prestamo')->references('id')->on('tabla_maestras');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestamos');
+    }
+}
