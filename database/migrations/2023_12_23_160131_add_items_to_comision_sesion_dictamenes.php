5cdc22121ae94b25e6a8aaf8ab3bf8472c81db54
Freddy <frimacc@gmail.com>
2023-12-23 11:35:03 -0500

    act codigo 23122023 1

--- database/migrations/2023_12_23_160131_add_items_to_comision_sesion_dictamenes.php
+++ database/migrations/2023_12_23_160131_add_items_to_comision_sesion_dictamenes.php
@@ -15,20 +15,20 @@ class AddItemsToComisionSesionDictamenes extends Migration
     {
         Schema::table('comision_sesion_dictamenes', function (Blueprint $table) {
             $table->bigInteger('unidad_inmobiliaria')->nullable();
-            $table->varchar('numero_expediente',100)->nullable();
+            $table->string('numero_expediente',100)->nullable();
             $table->date('fecha_ingreso')->nullable();
             $table->bigInteger('pisos')->nullable();
             $table->double('altura')->nullable();
             $table->double('area')->nullable();
             $table->bigInteger('id_numero_revision')->nullable();
             $table->bigInteger('id_apela_recon')->nullable();
-            $table->varchar('opinion_presidente',1)->nullable();
-            $table->varchar('opinion_presidente_desc',5000)->nullable();
-            $table->varchar('opinion_delegado',1)->nullable();
+            $table->string('opinion_presidente',1)->nullable();
+            $table->string('opinion_presidente_desc',5000)->nullable();
+            $table->string('opinion_delegado',1)->nullable();
             $table->Integer('id_opinion_delegado')->nullable();
             $table->Integer('id_instancia')->nullable();
-            $table->varchar('opinion_delegado_desc',5000)->nullable();
-            $table->varchar('notas_adicionales',5000)->nullable();
+            $table->string('opinion_delegado_desc',5000)->nullable();
+            $table->string('notas_adicionales',5000)->nullable();
             $table->bigInteger('id_dictamen')->nullable();
 
           //  id_comision_sesion int4 NULL,
