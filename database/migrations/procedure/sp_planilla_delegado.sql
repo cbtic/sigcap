1d2ba01c3872d31dc54f12043964914c722aa86e
wili85 <wyamunaque.expertta@gmail.com>
2024-01-03 16:25:13 -0500

    avance 03012024

--- database/migrations/procedure/sp_planilla_delegado.sql
+++ database/migrations/procedure/sp_planilla_delegado.sql
@@ -31,66 +31,81 @@ declare
 	
 	p_id_computo_sesion integer;
 	p_id_planilla_delegado integer;
+	
+	p_importe_fondo_comun decimal;
+	v_dia varchar;
 
 begin
 		
 	idp:=0;
 	
+	select to_char(((date_trunc('month', ('01-'||p_mes||'-'||p_anio)::date) + interval '1 month') - interval '1 day'),'dd')::varchar into v_dia;
+	
 	select id into p_id_computo_sesion from computo_sesiones cs where anio=p_anio and mes=p_mes and estado='1';
+	
+	select sum(d.saldo)::decimal into p_importe_fondo_comun
+	from delegado_fondo_comuns d
+	inner join periodo_delegado_detalles p on p.id = d.id_periodo_delegado_detalle and p.id_periodo_delegado = d.id_periodo_delegado   
+	where extract(year from p.fecha)::varchar = p_anio 
+	and extract(month from  p.fecha)::varchar = p_mes::int::varchar; 
+	
 	
 	insert into planilla_delegados(id_regional,periodo,mes,estado,id_usuario_inserta,created_at,updated_at)
 	values (5,p_anio::int,p_mes::int,1,1,now(),now());
 	
 	p_id_planilla_delegado := (SELECT currval('planilla_delegados_id_seq'));
-
-	--select periodo,mes,estado,id_usuario_inserta,created_at,updated_at from planilla_delegados pd
 	
-	insert into planilla_delegado_detalles(id_planilla,id_comision_delegado,sesiones,id_usuario_inserta,adelanto)
-	select p_id_planilla_delegado,cd.id id_comision_delegado,count(*) sesiones,1,
-	(select sum(total_adelanto) from adelantos a where id_agremiado=cd.id_agremiado and fecha between ('01-'||p_mes||'-'||p_anio)::date and ('31-'||p_mes||'-'||p_anio)::date)adelanto
+	
+	insert into planilla_delegado_detalles(id_planilla,id_comision_delegado,
+	sesiones,
+	sub_total,
+	adelanto,
+	reintegro,
+	coordinador,
+	total_bruto_sesiones,
+	movilidad_sesion,
+	total_movilidad,
+	reintegro_asesor,
+	total_bruto,
+	ir_cuarta,
+	total_honorario,
+	descuento,
+	saldo,
+	id_usuario_inserta)
+	
+	select p_id_planilla_delegado,id_comision_delegado,sesion_mes_actual,sub_total,adelanto,reintegro,coordinador
+	,(sub_total+adelanto+reintegro+coordinador)total_bruto_sesiones
+	,movilidad_sesion,total_movilidad,reintegro_asesor
+	,((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)total_bruto
+	,(((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)*0.08) ir_cuarta
+	,(((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)-(((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)*0.08))total_honorario
+	,descuento
+	,((((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)-(((sub_total+adelanto+reintegro+coordinador)+total_movilidad+reintegro_asesor)*0.08))-descuento)saldo
+	,1
+	from (
+	select id_comision_delegado,sesion_mes_actual
+	,case when sesion_mes_actual>0 then (p_importe_fondo_comun/sesion_mes_actual) else 0 end sub_total
+	,adelanto
+	,case when sesion_meses_anteriores>0 then (p_importe_fondo_comun/sesion_meses_anteriores) else 0 end reintegro
+	,0 coordinador
+	,0 movilidad_sesion
+	,0 total_movilidad
+	,0 reintegro_asesor
+	,0 descuento
+	from (
+	select cd.id id_comision_delegado,
+	sum(case when to_char(cs.fecha_programado,'yyyy-mm')=cs2.anio||'-'||cs2.mes then 1 else 0 end)sesion_mes_actual,
+	sum(case when to_char(cs.fecha_programado,'yyyy-mm-dd')::date<(cs2.anio||'-'||cs2.mes||'-01')::date then 1 else 0 end)sesion_meses_anteriores,
+	coalesce((select sum(total_adelanto) from adelantos a where id_agremiado=cd.id_agremiado and fecha between ('01-'||p_mes||'-'||p_anio)::date and (v_dia||'-'||p_mes||'-'||p_anio)::date),0)adelanto
 	from comision_sesiones cs 
 	inner join comision_sesion_delegados csd on cs.id=csd.id_comision_sesion
 	inner join comision_delegados cd on csd.id_delegado=cd.id
+	inner join computo_sesiones cs2 on cs.id_computo_sesion=cs2.id
 	where id_computo_sesion=p_id_computo_sesion
 	and id_aprobar_pago=2
-	group by cd.id;
-
-	/*
-	v_fecha_desde=p_anio||'-01-01';
-	v_fecha_hasta=p_anio||'-12-31';
-	
-	For entradas_mes in
-	select fecha_dias::date from generate_series(v_fecha_desde::date, v_fecha_hasta::date, '1 month'::interval) fecha_dias
-	loop
-		
-		v_anio := to_char(entradas_mes.fecha_dias,'yyyy')::int;
-		v_mes := to_char(entradas_mes.fecha_dias,'mm')::int;
-		v_mes_ := to_char(entradas_mes.fecha_dias,'mm')::varchar;
-	
-		select last_day_month into v_last_day_month from last_day_month(v_anio, v_mes);
-		
-		for entradas in 
-		select id id_agremiado,id_persona 
-		from agremiados a
-		where to_char(fecha_colegiado,'yyyy')::int < p_anio::int
-		and id_categoria!='90'
-		
-		loop
-			
-			select id,importe,id_moneda, denominacion into v_id_concepto,v_importe,v_id_moneda, v_denominacion from conceptos c where codigo='00006' and periodo=v_anio::varchar;
-			
-			insert into agremiado_cuotas(id_agremiado,id_regional,id_concepto,id_moneda,periodo,mes,importe,fecha_venc_pago,observacion,id_situacion,id_exonerado,id_pronto_pago,id_seguro_plan,id_usuario_inserta)
-			values (entradas.id_agremiado,5,v_id_concepto,v_id_moneda,v_anio::varchar,v_mes_,v_importe,v_last_day_month::date,'cuota del mes',1,0,0,1,1);
-			
-			p_i_id_agremiado_cuota := currval('agremiado_cuotas_id_seq');
-			
-			insert into valorizaciones(id_modulo,pk_registro,id_concepto,id_agremido,id_persona,monto,id_moneda,fecha,fecha_proceso,estado,id_usuario_inserta,created_at,updated_at, descripcion)
-			values (2,p_i_id_agremiado_cuota,v_id_concepto,entradas.id_agremiado,entradas.id_persona,v_importe,v_id_moneda,v_last_day_month::date,now(),1,1,now(),now(), v_denominacion||' '||v_mes_||'-'||v_anio::varchar);
-		
-		end loop;
-		
-	end loop;
-	*/
+	group by cd.id
+	)R
+	)S;
 	
 	return idp;
 	/*EXCEPTION
