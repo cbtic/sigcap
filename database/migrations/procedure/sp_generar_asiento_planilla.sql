a5e4e32bc313a2c1efc0e86ca3e63b15c59c7037
Freddy Rimac <frimacc@gmail.com>
2024-04-02 17:22:51 -0500

    sp_generar_asiento_planilla act

--- database/migrations/procedure/sp_generar_asiento_planilla.sql
+++ database/migrations/procedure/sp_generar_asiento_planilla.sql
@@ -14,7 +14,7 @@ declare
 	_anio integer;
 	_ruc character varying;
 
-	_grupo character varying;
+	_grupo integer;
 
 	_id_periodo_comision integer;
 	_id_periodo_comision_detalle integer;
@@ -23,10 +23,16 @@ declare
 
 begin
 	
-	_grupo := '1';
+	
 
 	_id_periodo_comision:=(select id_periodo_comision from periodo_comision_detalles where denominacion = p_anio||p_mes);
 	_id_periodo_comision_detalle:=(select id from periodo_comision_detalles where denominacion = p_anio||p_mes);
+
+	select max(id_grupo) into _grupo from asiento_planillas p 
+	where p.id_periodo_comision = _id_periodo_comision and p.id_periodo_comision_detalle = _id_periodo_comision_detalle and p.estado = '1';
+
+	_grupo := _grupo + 1;
+
 
 	
 	For cursor_rh In	
@@ -34,33 +40,59 @@ begin
 		select pd.id, sub_total,  monto, adelanto, reintegro, total_bruto, ir_cuarta, total_honorario, descuento, 
 		saldo, id_agremiado, 
 		tipo_comprobante, numero_comprobante, fecha_comprobante, a.id_persona, 
-		p.apellido_paterno ||' '|| p.apellido_materno || ' ' || p.nombres ||'-'|| p_mes ||'-'|| _grupo  glosa
-		from planilla_delegado_detalles pd
+		p.apellido_paterno ||' '|| p.apellido_materno || ' ' || p.nombres ||'-'|| p_mes ||'-'|| _grupo::varchar  glosa
+		from planilla_delegados pl 
+		inner join planilla_delegado_detalles pd on pd.id_planilla = pl.id  
 		inner join agremiados a on a.id = pd.id_agremiado
-		inner join personas p on p.id = a.id_persona 		
+		inner join personas p on p.id = a.id_persona
+		
 		loop
  			
 			insert into  asiento_planillas
-				(id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
 				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle, centro_costo, presupuesto)				
 				select 
-				cursor_rh.id_persona, '63291', cursor_rh.total_bruto, 0,  cursor_rh.total_bruto/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
-				_id_periodo_comision, _id_periodo_comision_detalle, 1, cursor_rh.id,'2025601','01P2501';
+				1, cursor_rh.id_persona, '63291', cursor_rh.total_bruto, 0,  cursor_rh.total_bruto/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id, '2025601', '01P2501';
 	
 			insert into  asiento_planillas
-				(id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
 				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle)				
 				select 
-				cursor_rh.id_persona, '40172', 0, cursor_rh.ir_cuarta, cursor_rh.ir_cuarta/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
-				_id_periodo_comision, _id_periodo_comision_detalle, 1, cursor_rh.id;
+				1, cursor_rh.id_persona, '40172', 0, cursor_rh.ir_cuarta, cursor_rh.ir_cuarta/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id;
 
 			insert into  asiento_planillas
-				(id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
 				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle)				
 				select 
-				cursor_rh.id_persona, '4241', 0, cursor_rh.total_honorario, cursor_rh.total_honorario/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
-				_id_periodo_comision, _id_periodo_comision_detalle, 1, cursor_rh.id;			
+				1, cursor_rh.id_persona, '4241', 0, cursor_rh.total_honorario, cursor_rh.total_honorario/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id;			
+
 			
+			
+			insert into  asiento_planillas
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle)				
+				select 
+				2, cursor_rh.id_persona, '4241',cursor_rh.total_honorario, 0, cursor_rh.total_honorario/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id;	
+			
+			insert into  asiento_planillas
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle)				
+				select 
+				2, cursor_rh.id_persona, '1692', 0, cursor_rh.descuento, cursor_rh.descuento/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id;
+	
+			insert into  asiento_planillas
+				(id_tipo, id_persona, cuenta, debe, haber, equivalente, glosa, id_tipo_documento, id_moneda, tipo_cambio, id_usuario_inserta,  
+				id_periodo_comision, id_periodo_comision_detalle, id_grupo, id_planilla_delegado_detalle, codigo_financiero, medio_pago)				
+				select 
+				2, cursor_rh.id_persona, '104141', cursor_rh.total_honorario-cursor_rh.descuento, 0, (cursor_rh.total_honorario-cursor_rh.descuento)/3.71, cursor_rh.glosa, 1, 1, 3.73, 1, 
+				_id_periodo_comision, _id_periodo_comision_detalle, _grupo, cursor_rh.id, '110', '001';
+
+
 		End Loop;
 
 idp:=1;
