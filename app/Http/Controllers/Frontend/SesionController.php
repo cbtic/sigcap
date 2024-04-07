b97aa2c68496f25e97c010a7cb42b7d12717d4b5
unknown <wyamunaque.expertta@gmail.com>
2024-04-07 12:54:12 -0500

    nuevo

--- app/Http/Controllers/Frontend/SesionController.php
+++ app/Http/Controllers/Frontend/SesionController.php
@@ -521,8 +521,6 @@ class SesionController extends Controller
 	
 	public function send_delegado_sesion(Request $request){
 		
-		//exit($request->flag_titular_suplente);
-		
 		$id_user = Auth::user()->id;
 		
 		if($request->id == 0){
@@ -534,6 +532,7 @@ class SesionController extends Controller
 		if($request->flag_titular_suplente == 1){ //TITULAR
 		
 			$comisionDelegadoOld = ComisionDelegado::find($comisionSesionDelegado->id_delegado);
+			//echo $comisionDelegadoOld->id;exit();
 			$comisionDelegadoOld->estado = 0;
 			$comisionDelegadoOld->save();
 			
@@ -576,6 +575,16 @@ class SesionController extends Controller
 		$comisionSesionDelegado->estado = 1;
 		$comisionSesionDelegado->id_usuario_inserta = $id_user;
 		$comisionSesionDelegado->save();
+		
+		$comisionSesion = ComisionSesione::find($comisionSesionDelegado->id_comision_sesion);
+		$comisionSesionDelegado_model = new ComisionSesionDelegado();
+		$comisionSesionDelegados = $comisionSesionDelegado_model->getComisionDelegadosByIdDelegadoAndFecha($comisionDelegadoOld->id_agremiado,$comisionSesion->fecha_programado,$request->fecha_inicio_sesion,$request->fecha_fin_sesion);
+		
+		foreach($comisionSesionDelegados as $row){
+			$comisionSesionDelegadoObj = ComisionSesionDelegado::find($row->id);
+			$comisionSesionDelegadoObj->id_delegado = $id_delegado;
+			$comisionSesionDelegadoObj->save();
+		}
 			
     }
 	
