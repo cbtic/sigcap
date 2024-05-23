15035aa99b2b9e3de52df44939048e415cedc427
Julio Yamunaqué Cruz <julioyamunaque04@gmail.com>
2024-05-22 21:25:52 -0500

    act 22052024 2

--- app/Models/Propietario.php
+++ app/Models/Propietario.php
@@ -12,13 +12,20 @@ class Propietario extends Model
 
     function getPropietarioSolicitud($id_solicitud){      
 
-        $cad = "select tm.denominacion tipo_propietario, p2.numero_documento, p2.apellido_paterno ||' '|| p2.apellido_materno ||' '|| p2.nombres nombres, p2.numero_celular, p2.correo 
-        from propietarios p
-        inner join personas p2 on p.id_persona = p2.id
-        inner join solicitudes s on p.id_solicitud = s.id
-        inner join tabla_maestras tm on p.id_tipo_propietario ::int=tm.codigo::int and tm.tipo='110'
-        where s.id='".$id_solicitud."'
-        and p.estado='1'";
+        $cad = "select
+        CASE 
+             WHEN p.id_tipo_propietario = '78' THEN (select p2.numero_documento from personas p2 where p2.id = p.id_persona)
+             WHEN p.id_tipo_propietario = '79' THEN (select e.ruc from empresas e where e.id = p.id_empresa)
+       end as numero_documento, CASE 
+             WHEN p.id_tipo_propietario = '78' THEN (select p2.apellido_paterno||' '||p2.apellido_materno||' '||p2.nombres agremiado from personas p2 where p2.id = p.id_persona)
+             WHEN p.id_tipo_propietario = '79' THEN (select e.razon_social from empresas e where e.id = p.id_empresa)
+       end as propietario, tm.denominacion tipo_propietario
+       from propietarios p
+       --inner join personas p2 on p.id_persona = p2.id
+       inner join solicitudes s on p.id_solicitud = s.id
+       inner join tabla_maestras tm on p.id_tipo_propietario ::int=tm.codigo::int and tm.tipo='16'
+       where s.id='".$id_solicitud."'
+       and p.estado='1'";
 
         //echo $cad;
 		$data = DB::select($cad);
