5cae6d82e342cf27134fe8adbe341455ba4f5e93
Julio Yamunaqué Cruz <julioyamunaque04@gmail.com>
2024-06-08 17:35:37 -0500

    act 03062024 5

--- app/Models/UsoEdificacione.php
+++ app/Models/UsoEdificacione.php
@@ -27,6 +27,23 @@ class UsoEdificacione extends Model
         return $data;
     }
 
+    function getInfoSolicitudUso2($id_solicitud){      
+        $cad = "select s.id, ue.id id_uso_edificacion, tm.denominacion tipo_tramite, tm2.denominacion tipo_uso, ue.area_techada,
+		(select ruta_archivo from solicitud_documentos sd where id_solicitud=ue.id_solicitud and id_tipo_documento=1 and estado='1' order by 1 desc limit 1)ruta_archivo1,
+		(select ruta_archivo from solicitud_documentos sd where id_solicitud=ue.id_solicitud and id_tipo_documento=2 and estado='1' order by 1 desc limit 1)ruta_archivo2,
+		(select ruta_archivo from solicitud_documentos sd where id_solicitud=ue.id_solicitud and id_tipo_documento=3 and estado='1' order by 1 desc limit 1)ruta_archivo3 
+		from uso_edificaciones ue 
+        inner join solicitudes s on ue.id_solicitud = s.id 
+        inner join tabla_maestras tm on s.id_tipo_tramite = tm.codigo::int and  tm.tipo ='25'
+        inner join tabla_maestras tm2 on ue.id_tipo_uso = tm2.codigo::int and  tm2.tipo ='124'
+        where s.id='".$id_solicitud."'
+        and ue.estado ='1'";
+
+        //echo $cad;
+		$data = DB::select($cad);
+        return $data;
+    }
+
     function getInfoSolicitudUsoTipo($id_solicitud){      
         $cad = "select s.id, ue.id id_uso_edificacion, tm.denominacion tipo_tramite, (SELECT STRING_AGG(tm2.denominacion, ' - ') AS tipo_uso
         FROM uso_edificaciones ue
