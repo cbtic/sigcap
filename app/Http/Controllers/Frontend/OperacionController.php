d85f095b00c1eba8291e47a9662cb9fd6fa4efb9
unknown <wyamunaque.expertta@gmail.com>
2024-06-15 22:11:24 -0500

    nuevo..

--- app/Http/Controllers/Frontend/OperacionController.php
+++ app/Http/Controllers/Frontend/OperacionController.php
@@ -4,12 +4,47 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OperacionController extends Controller
 {
     
 	public function consulta(Request $request)
     {
+	
+		$validator = Validator::make($request->all(), [
+			'MESSAGE TYPE IDENTIFICATION' => 'required|numeric|digits_between:1,4',
+			'PRIMARY BIT MAP' => 'required|max:16',
+			'SECONDARY BIT MAP' => 'required|max:16',
+			'PRIMARY ACCOUNT NUMBER' => 'required|numeric|digits_between:1,19',
+			'PROCESSING CODE' => 'required|numeric|digits_between:1,6',
+			'AMOUNT TRANSACTION' => 'required|numeric|digits_between:1,12',
+			'TRACE' => 'required|numeric|digits_between:1,6',
+			'TIME LOCAL TRANSACTION' => 'required|numeric|digits_between:1,6',
+			'DATE LOCAL TRANSACTION' => 'required|numeric|digits_between:1,8',
+			'POS ENTRY MODE' => 'required|numeric|digits_between:1,3',
+			'POS CONDITION CODE' => 'required|numeric|digits_between:1,2',
+			'ACQUIRER INSTITUTION ID CODE' => 'required|numeric|digits_between:1,8',
+			'FORWARD INSTITUTION ID CODE' => 'required|numeric|digits_between:1,8',
+			'RETRIEVAL REFERENCE NUMBER' => 'required|max:12',
+			'CARD ACCEPTOR TERMINAL ID' => 'required|max:8',
+			'CARD ACCEPTOR ID CODE' => 'required|max:15',
+			'CARD ACCEPTOR NAME LOCATION' => 'required|max:40',
+			'TRANSACTION CURRENCY CODE' => 'required|numeric|digits_between:1,3',
+			'LONGITUD' => 'required|numeric|digits_between:1,4',
+			'CodigoEmpresa' => 'required|max:7',
+			'CodigoProducto' => 'required|numeric|digits_between:1,3',
+			'TipoConsulta' => 'required|max:1',
+			'NumConsulta' => 'required|max:14',
+			'NumPagina' => 'required|numeric|digits_between:1,2',
+			'NumMaxDocs' => 'required|numeric|digits_between:1,2',
+			'Filler' => 'max:30',
+		]);
+		//print_r($validator->messages());exit();
+		if ($validator->fails()) {
+			return response()->json(['message' => $validator->errors()], 200);
+		}
+		
 		$data["MESSAGE TYPE IDENTIFICATION"] = "0210";
 		$data["PRIMARY BIT MAP"] = "F03804818E808000";
 		$data["SECONDARY BIT MAP"] = "0000000000000080";
