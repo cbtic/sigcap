8bac9baea70b794639c54e52437fdbd9eec23406
Julio Yamunaqué Cruz <julioyamunaque04@gmail.com>
2024-06-20 12:23:57 -0500

    act 18062024 7

--- database/migrations/procedure/sp_listar_asignacion_cuenta_paginado.sql
+++ database/migrations/procedure/sp_listar_asignacion_cuenta_paginado.sql
@@ -1,5 +1,3 @@
--- DROP FUNCTION public.sp_listar_asignacion_cuenta_paginado(varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, varchar, refcursor);
-
 CREATE OR REPLACE FUNCTION public.sp_listar_asignacion_cuenta_paginado(p_cuenta character varying, p_denominacion character varying, p_tipo character varying, p_centro_costo character varying, p_partida_presupuestal character varying, p_cod_financiero character varying, p_medio_pago character varying, p_origen character varying, p_estado character varying, p_pagina character varying, p_limit character varying, p_ref refcursor)
  RETURNS refcursor
  LANGUAGE plpgsql
@@ -21,15 +19,18 @@ begin
 	p_pagina=(p_pagina::Integer-1)*p_limit::Integer;
 	
 	v_campos=' ac.id, pc.cuenta cuenta, ac.denominacion, tc.denominacion tipo_cuenta, 
-		cc.codigo centro_costo, pp.codigo partida_presupuestal, ac.id_codigo_financiero codigo_financiero, 
-		mp.codigo medio_pago, ac.id_origen origen, ac.estado ';
+	cc.codigo centro_costo, pp.codigo partida_presupuestal, cf.codigo || ''-'' ||cf.denominacion codigo_financiero, 
+	mp.codigo || ''-'' || mp.denominacion medio_pago, tm.codigo||''-''||tm.denominacion origen, tm2.denominacion tipo_planilla, ac.estado  ';
 
 	v_tabla=' from asignacion_cuentas ac 
-		left join plan_contables pc on pc.id = ac.id_plan_contable 
-		left join tabla_maestras tc on tc.tipo = ''122'' and tc.codigo::int = ac.id_tipo_cuenta::int 
-		left join centro_costos cc on cc.id = ac.id_centro_costo 
-		left join partida_presupuestales pp on pp.id = ac.id_partida_presupuestal 
-		left join tabla_maestras mp on mp.tipo = ''108'' and mp.codigo::int = ac.id_medio_pago::int ';
+	left join plan_contables pc on pc.id = ac.id_plan_contable 
+	left join tabla_maestras tc on tc.tipo = ''122'' and tc.codigo::int = ac.id_tipo_cuenta::int 
+	left join centro_costos cc on cc.id = ac.id_centro_costo 
+	left join partida_presupuestales pp on pp.id = ac.id_partida_presupuestal 
+	left join tabla_maestras mp on mp.tipo = ''108'' and mp.codigo::int = ac.id_medio_pago::int
+	left join codigo_financieros cf on ac.id_codigo_financiero = cf.id
+	left join tabla_maestras tm on tm.tipo = ''128'' and tm.codigo::int = ac.id_origen ::int
+	left join tabla_maestras tm2 on tm2.tipo = ''129'' and tm2.codigo::int = ac.id_tipo_planilla ::int';
 	
 	v_where = ' Where 1=1  ';
 	
@@ -38,7 +39,7 @@ begin
 	End If;
 	
 	If p_denominacion<>'' Then
-	 v_where:=v_where||'And ac.denominacion ilike '''||p_denominacion||'%'' ';
+	 v_where:=v_where||'And ac.denominacion ilike ''%'||p_denominacion||'%'' ';
 	End If;
 
 	If p_tipo<>'' Then
@@ -62,7 +63,7 @@ begin
 	End If;
 
 	If p_origen<>'' Then
-	 v_where:=v_where||'And ac.id_origen ilike '''||p_origen||'%'' ';
+	 v_where:=v_where||'And ac.id_origen = '''||p_origen||''' ';
 	End If;
 
 
