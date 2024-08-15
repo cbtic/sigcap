6d526cdf3e2afe812fdd429169ddeaabbdc3ff1a
unknown <wyamunaque.expertta@gmail.com>
2024-08-15 00:09:17 -0500

    nuevo.....

--- app/Http/Controllers/Frontend/ReporteController.php
+++ app/Http/Controllers/Frontend/ReporteController.php
@@ -125,7 +125,7 @@ class ReporteController extends Controller
 		ini_set('display_errors', 1);
 		ini_set('display_startup_errors', 1);
 		error_reporting(E_ALL);
-		ini_set('memory_limit', '1280M');
+		ini_set('memory_limit', '-1');
 		ini_set('max_execution_time', '600');
 		
 		$reporte = Reporte::find($id);
