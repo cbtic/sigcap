0927ecddcec5af2ab82ec3632e250a73f1298c76
unknown <wyamunaque.expertta@gmail.com>
2024-11-22 01:53:10 -0500

    avance responsable

--- resources/views/frontend/auth/registerResp.blade.php
+++ resources/views/frontend/auth/registerResp.blade.php
@@ -25,9 +25,9 @@
                                         <label class="form-check-label" for="inlineRadio2">Responsable del tramite</label>
                                     </div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-md-4 mt-2">
                                     <label>Tipo de Documento:</label>
-                                    <select name="id_tipo_documento" id="id_tipo_documento" class="form-control" type="text">
+                                    <select name="id_tipo_documento" id="id_tipo_documento" class="form-control" onchange="validarTipoDocumento()">
                             			<!--<option value="">--Seleccionar--</option>-->
 										<?php
 										foreach ($tipo_documento as $row) {
@@ -39,34 +39,47 @@
                                     </select>
                                     
                                 </div>
-                                <div class="col-md-4" id="names_div">
-                                    <label id="names-title">Nombres:</label>
-                                    <input type="text" name="nombre" class="form-control upper-case just-letters" maxlength="100" required="" id="nombre">
+
+                                <div class="col-md-4 mt-2">
+                                    <label id="lbl_numero_documento">DNI:</label>
+                                    <input type="number" name="numero_documento" class="form-control other-doc-case dni-case" maxlength="15"  id="numero_documento" onchange="obtenerDatosDni()">
                                     
                                 </div>
-                                <div class="col-md-4" id="first_surname_div" style="display: block;">
+                                
+                            
+
+                                <div class="col-md-4 mt-2" id="names_div">
+                                    <label id="lbl_nombre">Nombres:</label>
+                                    <input type="text" name="nombre" class="form-control upper-case just-letters" maxlength="100"  id="nombre" readonly="readonly">
+                                    
+                                </div>
+
+                                <div class="col-md-4 mt-2 apellido" id="first_surname_div" style="display: block;">
                                     <label>Primer Apellido:</label>
-                                    <input type="text" name="apellido_paterno" class="form-control upper-case just-letters" maxlength="50" id="apellido_paterno">
+                                    <input type="text" name="apellido_paterno" class="form-control upper-case just-letters" maxlength="50" id="apellido_paterno" readonly="readonly">
                                     
                                 </div>
-                                <div class="col-md-4 mt-2" id="second_surname_div" style="display: block;">
+                                <div class="col-md-4 mt-2 apellido" id="second_surname_div" style="display: block;">
                                     <label>Segundo Apellido:</label>
-                                    <input type="text" name="apellido_materno" class="form-control upper-case just-letters" maxlength="50" id="apellido_materno">
+                                    <input type="text" name="apellido_materno" class="form-control upper-case just-letters" maxlength="50" id="apellido_materno" readonly="readonly">
                                     
                                 </div>
-                                <div class="col-md-4 mt-2 ">
-                                    <label id="identity_title">DNI:</label>
-                                    <input type="number" name="numero_documento" class="form-control other-doc-case dni-case" maxlength="15" required="" id="numero_documento">
-                                    
-                                </div>
-                                <div class="col-md-4 mt-2 ">
+                                
+                                <div class="col-md-4 mt-2">
                                     <label>Correo Electrónico:</label>
-                                    <input type="text" name="email" class="form-control" maxlength="50" required="" id="email">
+                                    <input type="text" name="email" class="form-control" maxlength="50" id="email">
                                     
                                 </div>
+
+                                <div class="col-md-4 mt-2">
+                                    <label>Correo Electrónico 2:</label>
+                                    <input type="email" name="email2" id="email2" class="form-control" placeholder="{{ __('E-mail Address') }}" value="{{ old('email') }}" maxlength="255" autocomplete="email" />
+                                    
+                                </div>
+
                                 <div class="col-md-4 mt-2">
                                     <label>Teléfono Celular:</label>
-                                    <input type="number" name="celular" class="form-control cellphone-case" min="900000000" max="999999999" maxlength="20" required="" id="celular">
+                                    <input type="number" name="celular" class="form-control cellphone-case" min="900000000" max="999999999" maxlength="20" id="celular">
                                     
                                 </div>
                                 <div class="col-md-4 mt-2">
@@ -77,6 +90,31 @@
                                 <div class="col-md-4 mt-2" id="address_div">
                                     <label id="address_title">Dirección:</label>
                                     <input type="text" name="direccion" class="form-control upper-case" maxlength="200" id="direccion">
+                                    
+                                </div>
+                            </div>
+
+                            <div class="form-row mb-2 mt-2">
+                                <div class="col-md-4 foto">
+                                    <label class="mb-3">Fotografía:</label>
+                                    <input type="file" name="foto" class="form-control-file" accept="image/*" id="foto">
+                                    
+                                </div>
+                                
+                                <div class="col-md-4">
+                                    <label>Firma:</label>
+                                    <input type="file" name="ruta_firma" class="form-control-file" accept="image/*" required="" id="ruta_firma">
+                                </div>
+                                
+                                <div class="col-md-4 ">
+                                    
+                                    <img src="#" id="peview" width="100%" style="display: none;">
+                                    
+                                    
+                                </div>
+                                <div class="col-md-8 offset-md-4 foto">
+                                    
+                                    <span>Suba una imágen escaneada de su sello y firma, los cuales será utilizados como validación de las solicitudes que realice através del portal</span>
                                     
                                 </div>
                             </div>
@@ -326,39 +364,47 @@ function obtenerDatosDni(){
     $('.loader').css("opacity","0.8").css("height",heightBrowser).html("<div id='Grd1_wrapper' class='dataTables_wrapper'><div id='Grd1_processing' class='dataTables_processing panel-default'>"+msgLoader+"</div></div>");
     $('.loader').show();
     
-    $.ajax({
-        url: '/persona/obtener_datos_persona/' + numero_documento,
-        dataType: "json",
-        success: function(result){
-            var persona = result.persona;
+    if(id_tipo_documento==1){
 
-            if(persona!="0")
-            {
-                $('#nombre').val(persona.nombre);
-                $('#apellido_paterno').val(persona.apellido_paterno);
-                $('#apellido_materno').val(persona.apellido_materno);
-                $('.loader').hide();
-                
-            }else{
-                //alert("ok");
-                $('#nombre').val("");
-                $('#apellido_paterno').val("");
-                $('#apellido_materno').val("");
-                $('.loader').hide();
-                validaDni(numero_documento);
-                
+        $.ajax({
+            url: '/persona/obtener_datos_persona/' + numero_documento,
+            dataType: "json",
+            success: function(result){
+                var persona = result.persona;
+
+                if(persona!="0")
+                {
+                    $('#nombre').val(persona.nombre);
+                    $('#apellido_paterno').val(persona.apellido_paterno);
+                    $('#apellido_materno').val(persona.apellido_materno);
+                    $('.loader').hide();
+                    
+                }else{
+                    //alert("ok");
+                    $('#nombre').val("");
+                    $('#apellido_paterno').val("");
+                    $('#apellido_materno').val("");
+                    $('.loader').hide();
+                    validaDni(numero_documento);
+                    
+                }
+
+                if (msg != "") {
+                    bootbox.alert(msg);
+                    return false;
+                }
+
+
             }
-
-            if (msg != "") {
-                bootbox.alert(msg);
-                return false;
-            }
-
-
-        }
+            
+        });
         
-    });
-    
+    }
+
+    if(id_tipo_documento==2){
+        validaRuc(numero_documento)
+    }
+
 }
 
 
@@ -418,5 +464,107 @@ function validaDni(dni) {
     });
 }
 
+function validarTipoDocumento(){
+    
+    var id_tipo_documento = $("#id_tipo_documento").val();
+    $(".apellido").hide();
+    //$(".divColegiatura").hide();
+    $(".foto").show();
+
+    $("#numero_documento").val("");
+    $("#nombre").val("");
+    $("#apellido_paterno").val("");
+    $("#apellido_materno").val("");
+
+    $("#nombre").prop("readonly",false);
+    $("#apellido_paterno").prop("readonly",false);
+    $("#apellido_materno").prop("readonly",false);
+
+    if(id_tipo_documento==1){
+
+        $("#nombre").prop("readonly",true);
+        $("#apellido_paterno").prop("readonly",true);
+        $("#apellido_materno").prop("readonly",true);
+
+        $(".apellido").show();
+        $("#lbl_numero_documento").html("DNI");
+        $("#lbl_nombre").html("Nombres");
+    }
+
+    if(id_tipo_documento==2){
+        $("#nombre").prop("readonly",true);
+        $("#lbl_numero_documento").html("RUC");
+        $("#lbl_nombre").html("Razón Social");
+        $(".foto").hide();
+    }
+
+    if(id_tipo_documento==3){
+        $(".apellido").show();
+        $("#lbl_numero_documento").html("Carné de Extranjería");
+        $("#lbl_nombre").html("Nombres");
+    }
+
+    if(id_tipo_documento==4){
+        $(".apellido").show();
+        $("#lbl_numero_documento").html("No Domiciliario");
+        $("#lbl_nombre").html("Nombres");
+    }
+
+    if(id_tipo_documento==5){
+        $(".apellido").show();
+        $("#lbl_numero_documento").html("Pasaporte");
+        $("#lbl_nombre").html("Nombres");
+    }
+
+    if(id_tipo_documento==6){
+        $(".apellido").show();
+        $("#lbl_numero_documento").html("Cédula");
+        $("#lbl_nombre").html("Nombres");
+    }
+
+}
+
+function validaRuc(ruc){
+	var settings = {
+		"url": "https://apiperu.dev/api/ruc/"+ruc,
+		"method": "GET",
+		"timeout": 0,
+		"headers": {
+		  "Authorization": "Bearer 20b6666ddda099db4204cf53854f8ca04d950a4eead89029e77999b0726181cb"
+		},
+	  };
+	  
+	  $.ajax(settings).done(function (response) {
+		console.log(response);
+		
+		if (response.success == true){
+
+			var data= response.data;
+
+			$('#nombre').val('')
+			$('#direccion').val('')
+			
+			$('#nombre').val(data.nombre_o_razon_social).attr('readonly', true);
+			//$('#nombre_comercial').val(data.nombre_o_razon_social).attr('readonly', true);
+			//$('#direccion').attr('readonly', true);
+
+			if (data.direccion_completa != ""){
+				$('#direccion').val(data.direccion_completa).attr('readonly', true);
+			}
+			else{
+				$('#direccion').attr('readonly', false);
+			}
+			
+			//alert(data.direccion_completa);
+
+		}
+		else{
+			Swal.fire("RUC Inv&aacute;lido. Revise el RUC digitado!");
+			return false;
+		}
+
+		
+	  });
+}
 
 </script>
