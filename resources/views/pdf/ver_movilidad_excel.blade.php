f827a60864ba559a8569e949fc7d33db8d8ec072
wili85 <wyamunaque.expertta@gmail.com>
2024-12-12 18:49:55 -0500

    nuevo.........

--- resources/views/pdf/ver_movilidad_excel.blade.php
+++ resources/views/pdf/ver_movilidad_excel.blade.php
@@ -186,7 +186,7 @@
 					foreach($meses as $keym=>$m){
 						$monto = 0;
 						//$movilidadMes = \App\Models\ComisionMovilidade::getMovilidadMesByPeriodoAndMunicipalidad($id_periodo,$anio,$m->mes_,$r->id_municipalidad_integrada);
-						if($m->mes_==$mes){
+						if((int)$m->mes_<=(int)$mes){
 							$movilidadMes = \App\Models\ComisionMovilidade::getMovilidadMesByPeriodoAndMunicipalidad($id_periodo,$anio,$m->mes_,$r->id_municipalidad_integrada);
 							$monto = (isset($movilidadMes->monto))?$movilidadMes->monto:"0";
 						}
