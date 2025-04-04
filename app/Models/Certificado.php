519d795a816a120abc13eb8e905b52fe2b188336
Cesar <julioyamunaque04@gmail.com>
2025-04-04 10:20:39 -0500

    act codigo certificado

--- app/Models/Certificado.php
+++ app/Models/Certificado.php
@@ -20,8 +20,8 @@ class Certificado extends Model
 
 	public function getCodigoCertificado($id_tipo){
 		
-		$cad = "select lpad((count(*)+1)::varchar,5,'0') codigo 
-from certificados c where id_tipo=".$id_tipo;
+		$cad = "select lpad((max(codigo::int)+1)::varchar,6,'0') codigo 
+        from certificados c where id_tipo".$id_tipo;
         
 		$data = DB::select($cad);
         return $data[0]->codigo;
