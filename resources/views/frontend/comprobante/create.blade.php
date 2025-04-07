4b7bacc81316541571395b50d357552ae79df17d
Freddy Rimac Coral <frimacc@gmail.com>
2025-04-07 11:13:01 -0500

    Act comprobate tipo de operacion

--- resources/views/frontend/comprobante/create.blade.php
+++ resources/views/frontend/comprobante/create.blade.php
@@ -556,7 +556,7 @@
                                                             <option value="">--Selecionar--</option>
                                                             <?php
                                                             foreach ($tipooperacion as $row) { ?>
-                                                                <option value="<?php echo $row->codigo ?>" <?php if ($row->codigo == 1) echo "selected='selected'" ?>><?php echo $row->denominacion ?></option>
+                                                                <option value="<?php echo $row->codigo ?>" <?php if ($row->codigo == '0101') echo "selected='selected'" ?>><?php echo $row->denominacion ?></option>
                                                             <?php
                                                             }
                                                             ?>
