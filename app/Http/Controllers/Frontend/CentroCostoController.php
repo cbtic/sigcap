9385d2ec1cd6d623061b4044202ac3fa5e7b34d3
unknown <wyamunaque.expertta@gmail.com>
2025-05-12 15:57:13 -0500

    nuevo

--- app/Http/Controllers/Frontend/CentroCostoController.php
+++ app/Http/Controllers/Frontend/CentroCostoController.php
@@ -47,6 +47,29 @@ class CentroCostoController extends Controller
 				$centroCosto->id_usuario_inserta = 1;
 				$centroCosto->save();
 			}
+
+			$centroCostoExiste = CentroCosto::where("codigo",$row->CODIGO)->where("denominacion",$row->NOM)->where("estado",1)->get();
+
+			if(count($centroCostoExiste)==0){
+
+				$centroCostoAnt = CentroCosto::where("codigo",$row->CODIGO)->where("estado",1)->first();
+				if($centroCostoAnt){
+					$centroCostoAnt->estado = 0;
+					$centroCostoAnt->save();
+				}
+
+				$centroCosto = new CentroCosto;
+				$centroCosto->id_regional=5;
+				$centroCosto->periodo=date("Y");
+				$centroCosto->codigo = $row->CODIGO;
+				$centroCosto->denominacion = $row->NOM;
+				$centroCosto->total_debe = ($row->TDEBE!="")?$row->TDEBE:0;
+				$centroCosto->total_haber = ($row->THABER!="")?$row->THABER:0;
+				$centroCosto->estado = 1;
+				$centroCosto->id_usuario_inserta = 1;
+				$centroCosto->save();
+
+			}
 			
 		}
 		
