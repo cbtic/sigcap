5f37b26bc88a35eb236649d630ca28cdf1599c68
Cesar <julioyamunaque04@gmail.com>
2025-08-21 12:01:14 -0500

    act caja credito

--- resources/views/frontend/comprobante/modal_credito_pago.blade.php
+++ resources/views/frontend/comprobante/modal_credito_pago.blade.php
@@ -188,7 +188,7 @@ function datatablenewPago(){
             var iNroPagina 	= parseFloat(fn_util_obtieneNroPagina(aoData[3].value, aoData[4].value)).toFixed();
             var iCantMostrar 	= aoData[4].value;
 			
-			var id = $('#id').val();
+			var id = $('#id_registro').val();
 			//var denominacion = $('#nombre').val();
 			var estado = $('#estado').val();
 			
@@ -399,7 +399,7 @@ function fn_save(id_caja){
     
 	var _token = $('#_token').val();
 	var id = $('#id').val();	
-	var id_comprobante = $('#id').val();
+	var id_comprobante = $('#id_registro').val();
 	var id_medio = $('#id_medio').val();	
 	var fecha = $('#fecha').val();
 	var nro_operacion = $('#nro_operacion').val();	
@@ -480,8 +480,8 @@ function fn_save(id_caja){
             	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding-top:10px">
 					
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<input type="hidden" name="id" id="id" value="<?php echo $id?>">
-					<!--<input type="hidden" name="id" id="id" value="0">-->
+					<input type="hidden" name="id_registro" id="id_registro" value="<?php echo $id?>">
+					<input type="hidden" name="id" id="id" value="0">
 
 					<div class="row">
 						<div class="col-lg-1 col-md-12 col-sm-12 col-xs-12">
