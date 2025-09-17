4f3188a2efdb576ac1e154f30d157cc82dc32813
wili <wyamunaque@saludpol.gob.pe>
2025-09-17 11:50:39 -0500

    pago..2

--- resources/views/frontend/carrito/lista_pedido.blade.php
+++ resources/views/frontend/carrito/lista_pedido.blade.php
@@ -22,17 +22,17 @@ foreach ($pedido as $key => $row):
 	$n++;
 	$disabled = "";
 	$data = json_decode($row->response);
-	$c = preg_split('//', $data->dataMap->TRANSACTION_DATE, -1, PREG_SPLIT_NO_EMPTY);
+	if(isset($data->dataMap))$c = preg_split('//', $data->dataMap->TRANSACTION_DATE, -1, PREG_SPLIT_NO_EMPTY);
 ?>
 	<tr style="font-size:13px" data-toggle="tooltip" data-placement="top">
 		<td class="text-left"><?php echo $row->purchase_number ?></td>
 		<td class="text-left"><?php 
 			//echo date("d/m/Y", strtotime($row->fecha_proceso)) 
-			echo $c[4].$c[5]."/".$c[2].$c[3]."/".$c[0].$c[1]." ".$c[6].$c[7].":".$c[8].$c[9].":".$c[10].$c[11]; 
+			if(isset($data->dataMap))echo $c[4].$c[5]."/".$c[2].$c[3]."/".$c[0].$c[1]." ".$c[6].$c[7].":".$c[8].$c[9].":".$c[10].$c[11]; 
 		?></td>
 		<td class="text-left"><?php echo $row->email ?></td>
-		<td class="text-left"><?php echo $data->dataMap->CARD." (".$data->dataMap->BRAND.")"; ?></td>
-		<td class="text-left"><?php echo $data->order->amount. " ".$data->order->currency; ?></td>
+		<td class="text-left"><?php if(isset($data->dataMap))echo $data->dataMap->CARD." (".$data->dataMap->BRAND.")"; ?></td>
+		<td class="text-left"><?php if(isset($data->order))echo $data->order->amount. " ".$data->order->currency; ?></td>
 		<td>
             <a class="btn btn-square link link-icon" href="pedido/show/{{$row->id}}" style="padding-left:35px!important;line-height:37px"><i class="fa fa-search" style="line-height:unset !important;"></i></a>
 		</td>
