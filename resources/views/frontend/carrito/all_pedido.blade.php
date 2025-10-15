a0413b5e4d43c49f7da4f2033ae5a34dd203e621
wili <wyamunaque@saludpol.gob.pe>
2025-10-15 16:41:01 -0500

    seguridad...

--- resources/views/frontend/carrito/all_pedido.blade.php
+++ resources/views/frontend/carrito/all_pedido.blade.php
@@ -331,7 +331,7 @@ div {
 
 @extends('frontend.layouts.app_carrito')
 
-@section('title', ' | ' . __('labels.frontend.contact.box_title'))
+@section('title', 'Carrito')
 
 @section('breadcrumb')
 <ol class="breadcrumb" style="padding-left:130px;margin-top:0px;background-color:#283659">
